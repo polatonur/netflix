9a39783b00866c7f91a2554d7b5b9a42e022ec36
onur <onurpolatplt@gmail.com>
2021-07-20 20:14:46 +0200

    font size problem fixed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,11 @@ function App() {
         );
       })}
       <footer>
-        <p>Made with React at Le Reacteur By Nono</p>
+        <p>
+          Made with <span>React</span> at{" "}
+          <a href="https://www.lereacteur.io/">Le Reacteur</a> By{" "}
+          <a href="https://github.com/polatonur">Onur</a>
+        </p>
       </footer>
     </div>
   );
